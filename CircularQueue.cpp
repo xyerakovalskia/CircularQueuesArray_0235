f608bf047d21989a7e6b2fd27add3f4f0ef380f9
xyerakovalskia <xyera123987@gmail.com>
2026-05-26 10:50:38 +0700

    mengembalikan array ke awal jika rear berada di posisi terakhir

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -40,3 +40,14 @@ public:
         }
         else
         {
+            // jika rear berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+    }
+
+    void remove()
+    {
