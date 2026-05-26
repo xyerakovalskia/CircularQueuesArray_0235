67c7b9a692afa073deba02327d550a4f37c162a8
xyerakovalskia <xyera123987@gmail.com>
2026-05-26 10:57:00 +0700

    jika front > rear, iterasi dari dront hingga akhir array

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -102,3 +102,11 @@ public:
         }
         else
         {
+            // jika FRONT > REAR, iterasi dari FRONT hingga akhir array
+            while (FRONT_Position <= max - 1)
+            {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+
+            FRONT_Position = 0;
