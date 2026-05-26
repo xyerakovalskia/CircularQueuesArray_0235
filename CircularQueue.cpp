cdf60dfbe2ff95d2269829df2598deb84a10f723
xyerakovalskia <xyera123987@gmail.com>
2026-05-26 10:53:56 +0700

    jika elemen yg dihapus di terakhir maka kembali ke awal array

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -68,3 +68,10 @@ public:
         }
         else
         {
+            // jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
+    }
