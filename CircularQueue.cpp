0af768f0c5fc41599808d9b8422d9cdc57dc1701
xyerakovalskia <xyera123987@gmail.com>
2026-05-26 11:00:09 +0700

    menambahkan case 3 dan 4

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -153,3 +153,12 @@ int main()
                 q.remove();
                 break;
             }
+            case '3':
+            {
+                q.display();
+                break;
+            }
+            case '4':
+            {
+                return 0;
+            }
