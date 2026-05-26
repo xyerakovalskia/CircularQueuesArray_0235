7485b760c4969bb2d3212da272a6b28fcc5ddb05
xyerakovalskia <xyera123987@gmail.com>
2026-05-26 10:51:39 +0700

    menambahkan void remove dan mengecek antrian apakah kosong

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -51,3 +51,11 @@ public:
 
     void remove()
     {
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
