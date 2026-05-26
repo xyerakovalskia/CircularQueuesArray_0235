9395b66aa4c69c97df1c5ae06a6b6f1636181f09
xyerakovalskia <xyera123987@gmail.com>
2026-05-26 10:58:47 +0700

    int main (while)

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -121,3 +121,21 @@ public:
         }
     }
 };
+
+int main()
+{
+    Queues q;
+    char ch;
+
+    while (true)
+    {
+        try
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement Delete Operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit " << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
