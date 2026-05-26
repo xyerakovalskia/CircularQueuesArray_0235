f159937752b1c8109c4878f723462230f36bf525
xyerakovalskia <xyera123987@gmail.com>
2026-05-26 10:54:50 +0700

    menambahkan void display dan mengecek apakah antrian kosong

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -75,3 +75,17 @@ public:
                 FRONT = FRONT + 1;
         }
     }
+
+    void display()
+    {
+        int FRONT_Position = FRONT;
+        int REAR_Position = REAR;
+
+        // cek apakah antrian kosong
+        if (FRONT_Position == -1)
+        {
+            cout << "Queue is empty\n";
+            return;
+        }
+
+        cout << "\nElements in the queue are....\n";
